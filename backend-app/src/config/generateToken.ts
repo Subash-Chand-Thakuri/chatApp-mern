30a5aeb9804eeedfe862676ec36ab17f76ae31a4
Subash Chand Thakuri <subashthakuri799@gmail.com>
2024-05-17 23:00:15 +0545

    Addedjwt

--- backend-app/src/config/generateToken.ts
+++ backend-app/src/config/generateToken.ts
@@ -1,8 +1,8 @@
-import jwt, { Secret } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 
 const generateToken = (id:string) => {
-    return jwt.sign({id},process.env.JWT_SECRET! as Secret, {
+    return jwt.sign({id},process.env.JWT_SECRET! , {
         expiresIn: "30d",
 
     })
